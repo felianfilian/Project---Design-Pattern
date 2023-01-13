093aa94ab2c67a73be758efb1a3716bc6a10a2cb
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-01-13 13:13:07 +0100

    factory function

--- factory.js
+++ factory.js
@@ -8,4 +8,17 @@ function Tester(name) {
   this.type = "Test";
 }
 
+function FactoryPattern() {
+  this.create = (name, type) => {
+    switch (type) {
+      case 1:
+        return new Developer(name);
+        break;
+      case 2:
+        return new Tester(name);
+        break;
+    }
+  };
+}
+
 Developer("Mario");
